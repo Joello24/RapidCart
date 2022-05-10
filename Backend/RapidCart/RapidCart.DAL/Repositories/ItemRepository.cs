47cb2d429d2ef4b9e8966e5b2fef0d6366d3255f
Gemeng Vang <gemengvang96@gmail.com>
2022-05-10 15:58:22 -0500

    Updated items inventory to int and completed repository plus testing

---
+++ Backend/RapidCart/RapidCart.DAL/Repositories/ItemRepository.cs
@@ -0,0 +1,112 @@
+﻿using RapidCart.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidCart.DAL.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private DBFactory _dbFactory;
+        public ItemRepository(DBFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public Response Delete(int itemId)
+        {
+            Response response = new Response();
+            using (var db = _dbFactory.GetDbContext())
+            {
+                try
+                {
+                    
+                    var item = db.Item.Where(x => x.ItemId == itemId).FirstOrDefault();
+                    db.Item.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    response.Success = false;
+                    response.Message = ex.Message;
+                    return response;
+                }
+                response.Success = true;
+                return response;
+
+             }
+        }
+
+        public Response<Item> Get(int itemId)
+        {
+            Response<Item> response = new Response<Item>();
+            using (var db = _dbFactory.GetDbContext())
+            {
+                try
+                {
+                    response.Data = db.Item.Where(x => x.ItemId == itemId).FirstOrDefault();
+                    if (response.Data == null)
+                    {
+                        response.Message = "Item not found";
+                        response.Success = false;
+                        return response;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    response.Message = ex.Message;
+                    response.Success = false;
+                    return response;
+                }
+            }
+            response.Success = true;
+            return response;
+        }
+
+        public Response<Item> Insert(Item item)
+        {
+            Response<Item> response = new Response<Item>();
+            using (var db = _dbFactory.GetDbContext())
+            {
+                try
+                {
+                    response.Data = db.Item.Add(item).Entity;
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+
+                    response.Success = false;
+                    return response;
+                }
+            }
+            response.Success = true;
+            return response;
+        }
+
+        public Response Update(Item item)
+        {
+            Response response = new Response();
+            using (var db = _dbFactory.GetDbContext())
+            {
+                try
+                {
+                    db.Item.Update(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                    response.Success = false;
+                    return response;
+                }
+            }
+            response.Success = true;
+            return response;
+        }
+    }
+
+}
