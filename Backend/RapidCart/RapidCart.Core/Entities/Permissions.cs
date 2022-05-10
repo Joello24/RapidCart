7486309fc037fc2727218ea3d7e5d02396180ff3
Gemeng Vang <gemengvang96@gmail.com>
2022-05-10 10:34:21 -0500

    implemented entities

---
+++ Backend/RapidCart/RapidCart.Core/Entities/Permissions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidCart.Core.Entities
+{
+    public class Permissions
+    {
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; }
+    }
+}
