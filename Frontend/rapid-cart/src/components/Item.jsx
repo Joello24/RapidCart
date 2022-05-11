1b17832e4448cdffa0c21fc944e62baf48a645d9
Robert Lillyblad <robert.lillyblad@gmail.com>
2022-05-11 15:54:27 -0500

    Add count button to each item

--- Frontend/rapid-cart/src/components/Item.jsx
+++ Frontend/rapid-cart/src/components/Item.jsx
@@ -4,10 +4,14 @@ import {useState} from "react";
 function Item(props) {
 
     const [isInCart, setIsInCart] = useState(false);
+    const [count, setCount] = useState(1);
 
     const addToCart = () => {
         setIsInCart(true);
 
+    }
+    const incrementCount = () => {
+        setCount(count + 1);
     }
     return (
         <div className="relative border bg-white border-gray-200 rounded-xl">
@@ -33,17 +37,12 @@ function Item(props) {
                         <button onClick={props.add} className="py-3 w-9/12 bg-yellow-300 rounded-bl-xl text-white font-extrabold">Add to
                             cart
                         </button>
-                        <button
-                            className="py-3 w-3/12 bg-white rounded-br-xl text-yellow-400 font-extrabold flex justify-center">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M11.761 20.8538C9.5904 19.5179 7.57111 17.9456 5.73929 16.1652C4.45144 14.8829 3.47101 13.3198 2.8731 11.5954C1.79714 8.25031 3.05393 4.42083 6.57112 3.28752C8.41961 2.69243 10.4384 3.03255 11.9961 4.20148C13.5543 3.03398 15.5725 2.69398 17.4211 3.28752C20.9383 4.42083 22.2041 8.25031 21.1281 11.5954C20.5302 13.3198 19.5498 14.8829 18.2619 16.1652C16.4301 17.9456 14.4108 19.5179 12.2402 20.8538L12.0051 21L11.761 20.8538Z"
-                                    stroke="#200E32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M15.7394 7.05304C16.8047 7.39334 17.5616 8.34974 17.6562 9.47502"
-                                      stroke="#200E32" stroke-width="1.5" stroke-linecap="round"
-                                      stroke-linejoin="round"/>
-                            </svg>
+                        <button className="bg-red-600 px-2 mx-2 box-border text-white font-bold" onClick={incrementCount} >
+                            -
+                        </button>
+                        <span className="text-sm">{count}</span>
+                        <button className="bg-red-600 px-2 mx-2 box-border text-white font-bold" onClick={incrementCount} >
+                            +
                         </button>
                     </div>
             </div>
