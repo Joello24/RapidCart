795935d4a3d9406a08d5ef9a23fb1c2ff12b764b
Robert Lillyblad <robert.lillyblad@gmail.com>
2022-05-11 15:10:40 -0500

    Add get all items, map items to shop page grid. Discount working with turnary and calculations on frontend

--- Frontend/rapid-cart/src/components/Item.jsx
+++ Frontend/rapid-cart/src/components/Item.jsx
@@ -13,8 +13,9 @@ function Item(props) {
         <div className="relative border bg-white border-gray-200 rounded-xl">
             <div className="relative border bg-white border-gray-200 rounded-xl">
 			<span className="absolute top-0 left-0 text-sm bg-red-400 p-2 rounded-br-xl rounded-tl-xl">
-				{props.discount}
-
+				{
+                    props.discount < .10 ? props.discount.toString().slice(3, 4) : props.discount.toString().slice(2, 4)
+                }%
 			</span>
             <span hidden={!isInCart} className= "absolute top-0 right-0 text-sm bg-green-300 p-2 rounded-bl-xl">
                 <img className="h-12 w-12" src={AddedToCart} alt=""/>
@@ -23,9 +24,10 @@ function Item(props) {
                      src="https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png"
                      alt="" />
                     <div className="p-5">
+                        <h2 className="text-lg">Stock: {props.inventory}</h2>
                         <h2 className="text-lg">{props.name}</h2>
-                        <h4 className="text-gray-500 text-sm">${props.price} <span
-                            className="line-through text-sm text-red-500">${props.oldPrice}</span></h4>
+                        <h4 className="text-gray-500 text-sm">${props.price - (props.price * props.discount)} <span
+                            className="line-through text-sm text-red-500">${props.price}</span></h4>
                     </div>
                     <div className="flex">
                         <button onClick={addToCart} className="py-3 w-9/12 bg-yellow-300 rounded-bl-xl text-white font-extrabold">Add to
--- Frontend/rapid-cart/src/components/Shop.jsx
+++ Frontend/rapid-cart/src/components/Shop.jsx
@@ -1,6 +1,6 @@
 ﻿import Item from './Item';
-import {useState} from "react";
-
+import {useEffect, useState} from "react";
+const url = 'http://localhost:5000/api/item';
 
 
 function Shop (props) {
@@ -9,27 +9,38 @@ function Shop (props) {
 
     const [cart, setCart] = useState([]);
 
+    useEffect(() => {
+        GetItems();
+    }, [setItems]);
     const addToCart = (item) => {
         setCart([...cart, item]);
     }
 
+    const GetItems = () => {
+        const get = {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+            }
+        };
+        fetch(url, get)
+            .then(res => res.json())
+            .then(data => {
+                setItems(data);
+                console.log(data);
+            })
+            .catch(err => console.log(err));
+    }
+
+
+
     return (
         <div className="h-screen w-screen bg-gray-50 p-5">
             <div className="grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-5">
-                {/*{items.map(item => (*/}
-                {/*<Item name={item.name} discount={item.discount} price={item.price}/>*/}
-                {/*))}*/}
+                {items.map(item => (
+                <Item name={item.name} discount={.15} price={item.price} inventory={item.inventory} item={item}/>
+                ))}
                 {cart}
-                <Item name="Apple" discount="10%" price="100" oldPrice="90" item={this} addToCart={addToCart}/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
-                <Item name="Apple" discount="10%" price="100" oldPrice="90"/>
             </div>
         </div>
     )
