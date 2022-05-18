f4dde45e8ab0f83c0d47cecf1d998f0919c4cfff
Gemeng Vang <gemengvang96@gmail.com>
2022-05-18 15:36:13 -0500

    Completed Add and Delete for items in frontend

--- Frontend/rapid-cart/src/App.js
+++ Frontend/rapid-cart/src/App.js
@@ -52,7 +52,7 @@ function App() {
             },
             body: loginInput,
         };
-        fetch("http://localhost:5000/api/auth/login", req)
+        fetch("http://localhost:5051/api/auth/login", req)
             .then(response => {
                 if (response.status !== 200) {
                     console.log(`Bad status: ${response.status}`);
@@ -87,7 +87,7 @@ function App() {
             body: signUpInput,
         };
         function add() {
-            return fetch("http://localhost:5000/api/user", req)
+            return fetch("http://localhost:5051/api/user", req)
                 .then(response => {
                     if (response.status !== 200 && response.status !== 201) {
                         console.log(`Bad status: ${response.status}`);
@@ -109,7 +109,7 @@ function App() {
         })
     }
     const AddToCart = (item) => {
-        const cartItemUrl= "http://localhost:5000/api/cartitem";
+        const cartItemUrl= "http://localhost:5051/api/cartitem";
 
         const cartItemBody = JSON.stringify({
             "OrderId" : cartId,
@@ -152,7 +152,7 @@ function App() {
         });
     }
     const RemoveFromCart = (item) => {
-        const cartItemUrl= "http://localhost:5000/api/cartitem";
+        const cartItemUrl= "http://localhost:5051/api/cartitem";
 
         const cartItem = {
             method: "DELETE",
@@ -176,7 +176,7 @@ function App() {
         });
     }
     const ClearCart = () => {
-        const cartUrl= "http://localhost:5000/api/cart";
+        const cartUrl= "http://localhost:5051/api/cart";
         const cart = {
             method: "DELETE",
             headers: {
--- Frontend/rapid-cart/src/components/Shop.jsx
+++ Frontend/rapid-cart/src/components/Shop.jsx
@@ -1,6 +1,8 @@
 ﻿import Item from './Item';
 import {useEffect, useState} from "react";
 import Example from "./CategoryDropdown";
+import { Route } from 'react-router-dom';
+import AddItem from './AddItemForm';
 
 const url = 'http://localhost:5051/api/item';
 
@@ -20,6 +22,26 @@ function Shop (props) {
     const [searchResultBool, setSearchResultBool] = useState(false);
 
     const [sortByCategory, setSortByCategory] = useState('');
+
+    const[ admin, setadmin] = useState(true);
+    
+    const[addButtonHidden, setAddButtonHidden] = useState(true);
+
+    const handleAdmin = () => {
+        if(admin){
+            setadmin(false);
+            setAddButtonHidden(true);
+        }
+        else{
+        setadmin(!admin);
+        }
+    }
+    
+    const handleAdd = () => {
+        setAddButtonHidden(!addButtonHidden);
+        
+    }
+
 
     useEffect(() => {
         GetItems();
@@ -116,14 +138,89 @@ function Shop (props) {
     }
 
 
+
+    const RemoveItem = (item) =>{
+        console.log(item);
+        const init = {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+            },
+        };
+        fetch(url + "/" + item.itemId, init)
+                .then(response => {
+                    if (response.status !== 200 && response.status !== 201) {
+                        console.log(`Bad status: ${response.status}`);
+                        return Promise.reject("response is not 200 OK");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    GetItems();
+                })
+                .catch(err => console.log(err));
+        };
+        
+    
+
+    function AddNewItem(item){
+
+        const newBody = JSON.stringify({
+            "categoryId": 1,
+            "name": item.name,
+            "description": item.description,
+            "price": item.price,
+            "inventory": item.inventory,
+            "path": item.path,
+            "orderItem": null
+        
+        })
+
+        const init = {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+
+
+            },
+
+            body: newBody
+        };
+        fetch(url , init)
+                .then(response => {
+                    if (response.status !== 200 && response.status !== 201) {
+                        console.log(`Bad status: ${response.status}`);
+                        GetItems();
+                        return Promise.reject("response is not 200 OK");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    GetItems();
+                })
+                .catch(err => console.log(err));
+        };
+        
+    
+
+    
+
     return (
         <div className="">
             <div className="text-center font-bold text-6xl bg-green-100">
                 Item Shop
             </div>
-            <span className="float-right mr-10">
-                    <Example />
-            </span>
+                <span className="float-right mr-10">
+                     <button  onClick={handleAdmin} className="rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700">Admin</button>
+                     
+                     <button hidden={admin}  onClick={handleAdd} className="rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700" >Add Item</button>
+                    
+                 </span>
+                <span  hidden={addButtonHidden} className="float-right mr-10 ">
+                    <AddItem  AddItem ={AddNewItem} />
+                </span>
             <div className="bg-green-100 text-center mb-20 pb-20 ml-2 pt-10">
                 <input id="searchInput" placeholder="Search..." className="w-1/3 rounded py-2 border-b-2 border-gray-600 outline-none focus:border-green-400" type="text" />
                 <button className="bg-green-500 px-2 mx-2 box-border text-white font-bold" onClick={HandleChange}>search</button>
@@ -133,8 +230,9 @@ function Shop (props) {
 
             {/*TODO: DEACTIVATE ADD TO CART AFTER ITEM IS ALREADY ADDED TO CART*/}
             <div className="mt-10 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
+               
                 {items.map(item => (
-                    <Item key={item.itemId} name={item.name} discount={.15} price={item.price} inventory={item.inventory} path ={item.path} item={item} add={() => addToCart(item)} />
+                    <Item key={item.itemId} admin={admin} Delete={() => RemoveItem(item)} name={item.name} discount={.15} price={item.price} inventory={item.inventory} path ={item.path} item={item} add={() => addToCart(item)} />
                 ))}
             </div>
         </div>
